b6f97675fc5982282ce41c7b7833082b862c7095
SimplonCharleville <fabrice.duchenois@gmail.com>
2018-05-29 09:33:02 +0200

    test3

--- cn.php
+++ cn.php
@@ -13,11 +13,26 @@ if (!$con) {
 //echo 'Connected to MySQL';
 
 //INSERT
-//$sql = "INSERT INTO participants (id, titre, nom, prenom, email, remember) VALUES (null, 'Monsieur', 'Tester', 'Test', 'test@test.com', 1, CURRENT_TIMESTAMP)";
+//$sql = "INSERT INTO participants (titre, nom, prenom, email, remember, date_creation) VALUES ('Monsieur', 'Tester', 'Test', 'test@test.com', 1, CURRENT_TIMESTAMP)";
 //$sql = "INSERT INTO participants (titre, nom, prenom, email, remember, date_creation) VALUES ('Monsieur', 'Tester', 'Test', 'test@test.com', 1, now())";
 
 //UPDATE
-$sql = "UPDATE participants SET nom='Testeur' WHERE email='test@test.com'";
+//$sql = "UPDATE participants SET nom='Testeur' WHERE email='test@test.com'";
+$sql = "SELECT * from participants where email='$email'";
+$result = mysqli_query($con, $sql);
+
+$email="test@test.com";
+$nom="Testeur3";
+$prenom="Test3";
+
+if (mysqli_num_rows($result) > 0) {
+  while($row = mysqli_fetch_assoc($result)) {
+    //echo "Nom : " . $row["nom"]. "<br>";
+    $sql = "UPDATE participants SET nom='$nom', prenom='$prenom' WHERE email='$email'";
+  }
+} else {
+    echo "0 results";
+}
 
 if (mysqli_query($con, $sql)) {
     //INSERT
