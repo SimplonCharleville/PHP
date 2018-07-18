835d010e0af845cf173ced06c33bbef0d8bb1b3e
Simplon Charleville <38696381+SimplonCharleville@users.noreply.github.com>
2018-07-18 10:02:28 +0200

    Update index.php

--- db-access/index.php
+++ db-access/index.php
@@ -8,7 +8,7 @@
   // Connexion à la DB
   $dbh = null;
   try {
-    $dbh = new PDO("mysql:host=$host_name; dbname=$database;", $user_name, $password);
+    $dbh = new PDO("mysql:host=$host_name; dbname=$database;", $user_name, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
     echo "<p>Connexion au serveur MySQL établie avec succès via pdo.</p >";
   } catch (PDOException $e) {
     echo "Erreur!: " . $e->getMessage() . "<br/>";
